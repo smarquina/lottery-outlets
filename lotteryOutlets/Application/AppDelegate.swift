1a06bad2dbd410596a71754d2ac8388dba4e679f
Sergio Martín Marquina <smarquina@zenos.es>
2020-01-22 14:20:09 +0100

    removed unused code

--- lotteryOutlets/Application/AppDelegate.swift
+++ lotteryOutlets/Application/AppDelegate.swift
@@ -16,22 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-//        //Update Navigation bar colors
-//        if #available(iOS 13.0, *) {
-//            let navBarAppearance = UINavigationBarAppearance()
-//            navBarAppearance.configureWithOpaqueBackground()
-//            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-//            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-//            navBarAppearance.backgroundColor = .tlGreen
-//            UINavigationBar.appearance(whenContainedInInstancesOf: [UINavigationController.self]).standardAppearance = navBarAppearance
-//            UINavigationBar.appearance(whenContainedInInstancesOf: [UINavigationController.self]).scrollEdgeAppearance = navBarAppearance
-//
-//        } else {
-//            UINavigationBar.appearance().barTintColor = .tlGreen
-//            UINavigationBar.appearance().tintColor = .white
-//            UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-//        }
-
         return true
     }
 
